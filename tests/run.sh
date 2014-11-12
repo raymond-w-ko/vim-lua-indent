06016719daa80e092d14d93143049788fb6b768a
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-11-11 21:09:19 -0500

    actually only one file is necessary

--- tests/run.sh
+++ tests/run.sh
@@ -8,19 +8,25 @@ pass()
 
 fail()
 {
-  echo -ne "\e[0;31mPASS\e[0m "
+  echo -ne "\e[0;31mFAIL\e[0m "
   echo "$@"
 }
 
-for f in *.in.lua; do
-  basename="${f%.in.lua}"
-  INPUT="$f"
+for f in *.ok.lua; do
+  basename="${f%.ok.lua}"
+  INPUT="$basename.in.lua"
   OUTPUT="$basename.OUT.lua"
   COMP="$basename.ok.lua"
+
+  vim -c "normal 1000<<" -c "write $INPUT" -c "qa!" "$COMP"
   vim -c "normal ggVG=" -c "write $OUTPUT" -c "qa!" "$INPUT"
   if diff "$COMP" "$OUTPUT" >/dev/null; then
     pass $basename
   else
     fail $basename
+    echo expected:
+    cat $COMP
+    echo got:
+    cat $OUTPUT
   fi
 done
