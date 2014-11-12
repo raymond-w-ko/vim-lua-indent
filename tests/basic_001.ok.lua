d5c68b5181f875ba7846fd86a4d276db2df8c657
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-11-12 13:43:20 -0500

    more tests

--- tests/basic_001.ok.lua
+++ tests/basic_001.ok.lua
@@ -4,4 +4,18 @@ end
 
 if condition == true then
 end
+
+if condition == true then
+    local t = {
+    }
+
+    local t = {
+        asdf
+    }
+
+    local t = {
+        asdf,
+        foo,
+    }
+end
 -- vim: et sw=4 sts=4 ts=4
