e920abed4e0a27ce86dfceeef5b2a1ae284b3275
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-08-14 19:19:48 -0400

    revamp test case suite to run with different options on different directories

--- tests/run.sh
+++ tests/run.sh
@@ -19,17 +19,14 @@ fail()
 }
 
 VIM="vim -u NONE -U NONE -i NONE"
+LOG="results.diff"
 
 do_tests()
 {
   DIRECTORY=$1
   REMOVE_INDENT=$2
-  INDENT_AMOUNT=''
-  if [[ ! -z $3 ]]; then
-    INDENT_AMOUNT="-c 'set sw=$3 sts=$3 ts=$3 noet'"
-  fi
 
-  for f in $DIRECTORY/*.ok.lua; do
+  for f in $DIRECTORY*.ok.lua; do
     basename="${f%.ok.lua}"
     INPUT="$basename.in.lua"
     OUTPUT="$basename.OUT.lua"
@@ -40,26 +37,49 @@ do_tests()
     else
       $VIM -c "normal ggVG420<<" -c "write! $INPUT" -c "qa!" "$COMP"
     fi
-    $VIM \
-      -c "set nocompatible" \
-      -c "edit $INPUT" \
-      -c "syntax on" \
-      $INDENT_AMOUNT \
-      -c "source ../after/indent/lua.vim" \
-      -c "normal ggVG=" \
-      -c "write! $OUTPUT" \
-      -c "qa!"
+    if [[ -n $3 ]]; then
+      $VIM \
+        -c "set nocompatible" \
+        -c "edit $INPUT" \
+        -c "syntax on" \
+        -c "source ../after/indent/lua.vim" \
+        -c "set sw=$3 sts=$3 ts=$3 expandtab" \
+        -c "normal ggVG=" \
+        -c "write! $OUTPUT" \
+        -c "qa!"
+    else
+      $VIM \
+        -c "set nocompatible" \
+        -c "edit $INPUT" \
+        -c "syntax on" \
+        -c "source ../after/indent/lua.vim" \
+        -c "normal ggVG=" \
+        -c "write! $OUTPUT" \
+        -c "qa!"
+    fi
     if diff "$COMP" "$OUTPUT" &>/dev/null ; then
       pass $basename
     else
       fail $basename
-      diff -rupN $COMP $OUTPUT
-      exit 1
+      echo "--------------------------------------------------------------------------------" >> $LOG
+      echo "$OUTPUT" >> $LOG
+      echo "$COMP" >> $LOG
+      diff -rupN $COMP $OUTPUT >> $LOG
+      if [[ -e $4 ]]; then
+        exit 1
+      fi
     fi
   done
 }
 
-# do_tests "basic" 1
-# do_tests "basic_passthrough" 0
-# do_tests "tsukuyomi" 0
-do_tests "nmap" 0 2
+echo "" > $LOG
+
+do_tests "basic/" 1
+do_tests "basic_passthrough/" 0
+do_tests "tsukuyomi/" 0
+
+# problematics ones which are clearly wrong
+# do_tests "nmap/stdnse" 0 2
+# do_tests "nmap/anyconnect" 0 2
+
+do_tests "nmap/*" 0 2 1
